6205f15cada5d8cd3d925d48cb329d7b77fc4d7f
muskandineshmandloi <muskanmandloi14@gmail.com>
2026-04-30 10:33:57 +0530

    updated

--- Controllers/listings.js
+++ Controllers/listings.js
@@ -37,7 +37,7 @@ module.exports.postListing = async (req, res, next) => {
 
     try {
         const response = await fetch(
-            `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_KEY}&q=${encodeURIComponent(query)}&format=json&limit=1`
+            `https://us1.locationiq.com/v1/search?key=${process.env.LOCATIONIQ_KEY}&q=${encodeURIComponent(query)}&format=json&limit=1`
         );
 
         if (response.ok) {
